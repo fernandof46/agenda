119c34605eedbe9425adae28feb9397588347716
Fernando Ferreira <121515671+fernandof46@users.noreply.github.com>
2023-12-21 15:13:03 -0300

    primeiro commit

---
+++ app/src/main/java/com/youke/teste/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.youke.teste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun sortear (view: View){
+        val textoResultado = findViewById<TextView>(R.id.resultado)
+        val numero = Random().nextInt(11)
+        textoResultado.setText("número gerado $numero")
+    }
+}

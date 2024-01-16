3b6f8eb573daee734339dac6748de4c0680fa19b
Fernando Ferreira <121515671+fernandof46@users.noreply.github.com>
2024-01-16 16:00:09 -0300

    atualização 16.01

---
+++ app/src/main/java/com/youke/teste/Mensagem.kt
@@ -0,0 +1,8 @@
+package com.youke.teste
+
+class Mensagem {
+    fun enviar(){
+        val usuario = Usuario() //cria uma função Objeto
+        usuario.logar() //seta a função do java no objeto kotlin
+    }
+}
